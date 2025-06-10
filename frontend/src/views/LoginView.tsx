90343e24e38ba1a69013f6a30b20c15021e57e05
guilvance <magna3d2y@gmail.com>
2025-06-09 21:51:49 -0500

    Añadiendo validadcion en el formulario e implementando rutas en la pagina

---
+++ frontend/src/views/LoginView.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+export default function LoginView() {
+    return (
+        <>
+            <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
+            <nav className="mt-10">
+                <Link className="text-center text-white text-lg block"
+                    to="/register">¿No tienes una cuenta? Registrate</Link>
+            </nav>
+        </>
+    )
+}
